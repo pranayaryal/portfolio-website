fccb88e05b61cb70590f328dedbfb95afa75bafe
Pranay Aryal <drpranayaryal@gmail.com>
2021-03-05 06:37:58 +0000

    Initial commit

---
+++ components/Header.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { LinkPreview } from "@/components/LinkPreview";
+
+const Header = () => {
+  const [open, setOpen] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
+  const variants = {
+    hidden: { opacity: 0, y: "1vh" },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        delayChildren: 0.1,
+        staggerChildren: 0.1,
+      },
+    },
+    // transition: { staggerChildren: 0.5 },
+    exit: { opacity: 0, y: "1vh" },
+  };
+
+  const itemA = {
+    hidden: { opacity: 0, scale: 0.5, y: "1vh" },
+    show: { opacity: 1, scale: 1, y: 0 },
+  };
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  return (
+    <div className="flex flex-col md:flex-row justify-between items-start md:space-x-14  w-full mb-10">
+      <div className="flex md:w-3/4 flex-col mt-6">
+        <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-2 text-black dark:text-white">
+          Pranay Aryal
+        </h1>
+
+        <p className="text-zinc-600 dark:text-zinc-500">
+          Software Engineer
+        </p>
+      </div>
+
+    </div>
+  );
+};
+
+export default Header;
+
+export const Hyperlink = ({ link, text }) => {
+  return (
+    <a
+      href={link}
+      className="inline-block font-bold text-zinc-700 dark:text-zinc-200 transform hover:scale-105 transition duration-200"
+      target="__blank"
+    >
+      {text}
+    </a>
+  );
+};
---
+++ pages/index.js
@@ -0,0 +1,53 @@
+import Container from "@/components/Container";
+import Link from "next/link";
+import BlogPost from "@/components/BlogPost";
+import ProjectCard from "@/components/ProjectCard";
+import Timeline from "@/components/Timeline";
+import Contact from "@/components/Contact";
+
+import { LIGHT_COLORS } from "@/lib/constants";
+
+import { shuffleArray } from "@/lib/shuffleArray";
+import { useEffect, useState } from "react";
+import { useIsFontReady } from "@/lib/useIsFontReady";
+
+import { useTheme } from "next-themes";
+import Talks from "@/components/Talks";
+import Header from "@/components/Header";
+
+export default function Home() {
+  const [colors, setColors] = useState([]);
+
+  let tempInterval;
+
+  const isFontReady = useIsFontReady();
+  const { theme, setTheme } = useTheme();
+
+  const play = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
+  useEffect(() => {
+    setColors(shuffleArray(LIGHT_COLORS));
+  }, []);
+
+  return (
+    <Container
+      title="Pranay Aryal - Developer"
+      description="Full-Stack developer"
+    >
+      <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
+        <Header />
+        <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-4 text-black dark:text-white">
+          Recent Blogs
+        </h3>
+        <BlogPost
+          title="PubMed Api"
+          summary="Cleaner explanation of pubMed Api"
+          slug="pubmed-api"
+        />
+
+      </div>
+    </Container>
+  );
+}
