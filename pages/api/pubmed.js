0d73fa7ca77945ba972d1c4c9587b4e8573db0f0
pranayaryal <drpranayaryal@gmail.com>
2023-01-09 00:43:33 -0500

    add more

--- pages/api/pubmed.js
+++ pages/api/pubmed.js
@@ -1,16 +1,55 @@
-import { getExampleSearch } from '@/lib/pubmed'
 const apiKey = process.env.SENDGRID_API_KEY
 
 const API_KEY = process.env.PUBMED_API_KEY
+const BASE_URL = `https://eutils.ncbi.nlm.nih.gov/entrez/eutils/`
+const SEARCH_ENDPOINT = `esearch.fcgi?db=pubmed&retmode=json&sort=pub_date&term=`
+const SUMMARY_ENDPOINT = `esummary.fcgi?db=pubmed&version=2.0&retmode=json&api_key=${API_KEY}&id=`
+const SPELLING_ENDPOINT = `espell.fcgi?db=pubmed&term=rhuinitis`
+const RELATED_ENDPOINT = `elink.fcgi?dbfrom=pubmed&db=pubmed&id=`
+
+const getPmids = async (term) => {
+    // Get the pubMed Ids associated with the search
+    const url = `${BASE_URL}${SEARCH_ENDPOINT}${term}+randomized+controlled+trial[pubt]`
+    const response  = await fetch(url)
+
+    const search = await response.json()
+    return search
+}
+
+const getSummary = async (idlist) => {
+    const joined = idlist.toString();
+    const url = `${BASE_URL}${SUMMARY_ENDPOINT}${joined}`
+    const response  = await fetch(url)
+
+    const summary = await response.json()
+    return summary
+}
 
 export default async (req, res) => {
   try {
+    const { term } = req.query
     // console.log("REQ.BODY", req.body);
-    const { esearchresult, header } = await getExampleSearch()
+    const { esearchresult, header } = await getPmids(term)
     const { idlist } = esearchresult
-    const { count } = esearchresult 
-    console.log(esearchresult)
-    res.json({ esearchresult, idlist, header, error: ""})
+
+    const summary = await getSummary(idlist)
+    const {  result } = summary
+    const resultValues = Object.values(result)
+
+    let summaries = []
+
+    resultValues.map(r => {
+      if (r.uid != undefined){
+        // This wil eliminate the last part of the array
+        // which is nothing but the list uids
+        summaries.push(r)
+      }
+    })
+
+    console.log(summaries)
+
+
+    res.json({ summaries , error: ""})
   } catch (error) {
     // console.log(error);
     return res.status(error.statusCode || 500).json({ error: error.message });
--- pages/pubmed-demo.js
+++ pages/pubmed-demo.js
@@ -4,28 +4,43 @@ import React, { useEffect, useState } from "react";
 
 const Friday = () => {
   const [search, setSearch] = useState("")
-  const [ errorMessage, setErrorMessage ] = useState("")
-  const [ pmids, setPmids ] = useState([])
+  const [ buttonText, setButtonText ] = useState("Search")
+  const [errorMessage, setErrorMessage] = useState("")
+  const [articleSummaries, setArticleSummaries] = useState([])
+
+  const handleChange = (e) => {
+    setSearch(e.target.value)
+    setErrorMessage("")
+  }
 
   const handleSubmit = async () => {
+    setArticleSummaries([])
+    setButtonText("Searching ...")
+    if (search == "") {
+      setErrorMessage("You must enter a search term")
+      setButtonText("Search")
+      return
+    }
     setErrorMessage("")
     console.log(search)
-    const res = await fetch("/api/pubmed", {
+    const res = await fetch(`/api/pubmed?term=${search}`, {
       headers: {
         "Content-Type": "application/json",
       },
       method: "GET",
     });
 
-    const { error, esearchresult, header, idlist } = await res.json();
+    const { error, summaries } = await res.json();
     if (error) {
       setErrorMessage('There was an error')
+      setButtonText("Search")
       return
     }
-    console.log(`The response is ${idlist}`)
-    setPmids(idlist)
-    console.log(pmids)
- 
+
+    // Set the pubMedIds from response
+    setArticleSummaries(summaries)
+    setButtonText("Search")
+
 
   }
 
@@ -40,29 +55,43 @@ const Friday = () => {
       description="Testing Demo"
     >
       <div className="max-w-4xl mx-auto py-40">
-        <p className="mb-4">Let's search pubmed for a Randomized Controlled Trial</p>
-        { errorMessage && 
+        <p className="mb-4">Search pubmed for a Randomized Controlled Trial</p>
+        {errorMessage &&
           <p className="mt-4 mb-2 text-sm text-red-400">{errorMessage}</p>
         }
-        <input
-          type="text"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-          className="text-black rounded-md border border-indigo-200 py-1 px-2 focus:outline-none focus:border-gray-400 placeholder:text-sm  dark:bg-zinc-800 dark:border-gray-700 dark:text-white mb-1"
-          placeholder="Enter a search term"
-        />
-        <button
-          onClick={handleSubmit}
-          className="text-black w-full px-4 py-4 border-2 border-black  rounded-md font-bold text-xl dark:border-gray-700  mb-4"
-        ></button>
+        <div className="flex flex-row justify-left space-x-4">
+          <input
+            type="text"
+            value={search}
+            onChange={(e) => handleChange(e)}
+            className="text-black max-w-2/3 h-10 rounded-md border border-indigo-200 py-1 px-2 focus:outline-none focus:border-gray-400 placeholder:text-sm  dark:bg-zinc-800 dark:border-gray-700 dark:text-white mb-1"
+            placeholder="Enter a search term"
+          />
+          <button
+            onClick={handleSubmit}
+            className="text-black bg-indigo-300 max-w-1/4 px-2 py-2 border-2 border-black  rounded-md font-bold text-sm dark:border-gray-700  mb-4"
+          >{buttonText}</button>
+
+        </div>
 
         <div className="max-w-xl">
-        { pmids && 
-          pmids.map(id => {
-            return <span> {id},</span>
-          })
+          {articleSummaries &&
+            articleSummaries.map(summary => {
+              return (
+                <div>
+                  <a target="_blank"
+                    href={`https://pubmed.ncbi.nlm.nih.gov/${summary.uid}/`}>
+                    <p className="mt-4 text-sm text-gray-900" key={summary.uid}> {summary.title}</p>
+                    <span className="text-xs text-indigo-400">{summary.lastauthor} et al |</span>
+                    <span className="text-xs text-green-400"> {summary.pubdate} |</span>
+                    <span className="text-xs text-red-400"> {summary.source}</span>
+                  </a>
+                </div>
+              )
 
-        }
+            })
+
+          }
 
 
         </div>
